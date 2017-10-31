4bdbd120c73e15f7cb8a3236cf34fed9851fe9bf
liuchao <liuc@xinguangnet.com>
2017-10-31 15:55:11 +0800

    修改

--- HelloProject/index.android.js
+++ HelloProject/index.android.js
@@ -1,12 +1,8 @@
-//require('./Application');
-
 import React, {Component} from 'react';
 import {
     AppRegistry,
     } from 'react-native';
 
-//这里不能写var App = require('./src/Application'); 会出现问题
-//（小白刚学还不会，有路过的大神可以留言教一下）。
 import App from './Application';
 
 export default class Pagejump extends Component {
